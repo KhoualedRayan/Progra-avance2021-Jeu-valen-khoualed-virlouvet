b5b5a30e2aed0b3812a4932cceccf16c7bfc5c8a
Khoualed Rayan <rayan.khoualed3@etu.univ-lorraine.fr>
2021-09-23 11:36:57 +0200

    Quelques constantes

--- constante.h
+++ constante.h
@@ -1,1 +1,20 @@
-int test();
+/**
+ * \file constante.h
+ * \brief en-tête qui permet de rassembler toutes les constantes.
+ * \author Khoualed Rayan, Valen Thomas, Virlouvet Evann
+ * \version 1.0
+ * \date 23 septembre 2021
+*/
+
+#ifndef __CONSTANTE__H__
+#define __CONSTANTE__H__
+
+/**
+ * \brief Largeur de l'écran de jeu
+ */
+#define SCREEN_WIDTH 1920
+
+/**
+ * \brief Hauteur de l'écran de jeu
+ */
+#define SCREEN_HEIGHT 1080
